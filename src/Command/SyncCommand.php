a4ef95d8e8ed4e55d56d94374f59a33dfd9327c1
Ruin0x11 <ipickering2@gmail.com>
2021-06-26 13:27:56 +0300

    anime controller and normalizer

--- src/Command/SyncCommand.php
+++ src/Command/SyncCommand.php
@@ -115,6 +115,15 @@ class SyncCommand extends Command
             );
         }
 
+        $animes = $this->animeRepository->getOld($dateCheck, $limit);
+
+        $this->logger->info('Update old animes', ['count' => count($animes)]);
+        foreach ($animes as $anime) {
+            $this->sendDelayed(
+                new Message(json_encode(['type' => 'anime', 'id' => $anime->getId()->toString()]))
+            );
+        }
+
         return 0;
     }
 
